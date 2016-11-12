087fed633cd39a3957594fe05816c2ab74aa2e9b
imran <meghazi.imran@gmail.com>
2016-11-13 00:19:32 +0100

    PULL git add .

---
+++ src/Entity/Animation.cpp
@@ -0,0 +1,21 @@
+#include "Animation.hpp"
+
+Animation::Animation(sf::Texture* t)
+{
+  textur
+}
+
+void update(sf::Time)
+{
+  
+}
+
+void draw(sf::RenderWindow*)
+{
+  
+}
+
+sf::Sprite* getCurrentAnimation()
+{
+  
+}
--- src/Entity/Animation.hpp
+++ src/Entity/Animation.hpp
@@ -15,20 +15,21 @@ class Animation
   };
   
 public:
-  // texture, nb of animations per direction, false : 1 dir, true : 4, anim duration
-  Animation();
-  Animation(sf::Texture*,int,bool,sf::Time=sf::seconds(1));
+  // texture, anim duration
+  Animation(sf::Texture*, sf::Time=sf::seconds(1));
   void update(sf::Time);
   void draw(sf::RenderWindow*);
   sf::Sprite* getCurrentAnimation();
+
 private:
-  sf::Sprite** sprites;
+  sf::Sprite* sprite;
   sf::Vector2f pos;
-  sf::Time elapse;
+  sf::Time elapsed;
   sf::Time tick;
   int nb_columns;
   int nb_rows;
-  int current_anim;
+  int current_anim_x;
+  int current_anim_y;
 };
 
 #endif
