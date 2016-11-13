31b1d790c3b1d1a0b83a77fb50bacf0cd2c154c3
Léo Andéol <leo.andeol@gmail.com>
2016-11-13 04:50:49 +0100

    Animation

--- src/Entity/Animation.cpp
+++ src/Entity/Animation.cpp
@@ -1,23 +1,49 @@
 #include "Animation.hpp"
-/*
-Animation::Animation(sf::Texture* t, sf::Time time, int nbColumns, int nbRows, float x, float y) : pos(x,y)
+
+Animation::Animation(sf::Texture* t, int nbColumns, int nbRows, sf::Vector2f coord, sf::Time total) : pos(coord)
 {
-  sprite.setTexture(*t);
-  tick = time/nbColumns;
+  sprite = new sf::Sprite();
+  sprite->setTexture(*t);
+  sprite->setPosition(coord);
+  tick = total/(float)nbColumns;
   nb_columns = nbColumns;
   nb_rows = nbRows;
-  elapsed = 0;
+  elapsed = sf::seconds(0);
   current_anim_x = 0;
-  current_anix_y = 0;
+  current_anim_y = 0;
+  sprite_width = t->getSize().x / nbColumns;
+  sprite_height = t->getSize().y / nbRows;
 }
 
-void update(sf::Time)
+Animation::~Animation()
 {
-  
+
 }
 
-void draw(sf::RenderWindow*)
+void Animation::update(sf::Time delta)
 {
-  
-}*/
+  elapsed += delta;
+  while(elapsed>=tick)
+    {
+      elapsed-=tick;
+      current_anim_x++;
+      if(current_anim_x>=nb_columns) current_anim_x = 0;
+    }
+  sf::IntRect rect(current_anim_x * sprite_width, current_anim_y * sprite_height, sprite_width, sprite_height);
+  sprite->setTextureRect(rect);
+}
 
+void Animation::draw(sf::RenderWindow* w)
+{
+  w->draw(*sprite);
+}
+
+void Animation::setPosition(sf::Vector2f pos)
+{
+  sprite->setPosition(pos);
+}
+
+void Animation::move(sf::Vector2f pos)
+{
+  sprite->move(pos);
+}
--- src/Entity/Animation.hpp
+++ src/Entity/Animation.hpp
@@ -10,10 +10,13 @@ class Animation
   
 public:
   // texture, anim duration
-  Animation(sf::Texture*, sf::Time=sf::seconds(1)); // 1/nb image dans ligne textures
+  Animation(sf::Texture*,int,int,sf::Vector2f,sf::Time=sf::seconds(1)); // 1/nb image dans ligne textures
+  ~Animation();
   void update(sf::Time);
   void draw(sf::RenderWindow*);
   sf::Sprite* getCurrentAnimation();
+  void setPosition(sf::Vector2f);
+  void move(sf::Vector2f);
 
 private:
   sf::Sprite* sprite;
@@ -24,6 +27,8 @@ private:
   int nb_rows;
   int current_anim_x;
   int current_anim_y;
+  int sprite_width;
+  int sprite_height;
 };
 
 #endif
